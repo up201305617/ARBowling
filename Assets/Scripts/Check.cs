e9db7e75c456a01f0ed886cfc00d6f93adc9b2ce
Jorge <up201305617@fe.up.pt>
2017-10-13 13:40:00 +0100

    get visibility from hiro marker

---
+++ Assets/Scripts/Check.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Check : MonoBehaviour
+{
+    private ARMarker [] armarker;
+    private ARMarker ballMarkerScript;
+    private ARMarker pinMarkerScript;
+    public Transform artoolkit;
+    public Transform PinMarker;
+    public Transform BallMarker;
+    public Text distance;
+    public Text twoMarkers;
+
+    void Start()
+    {
+        distance.text = "";
+        twoMarkers.text = "";
+        armarker = artoolkit.GetComponents<ARMarker>();
+        GetMarkers();
+    }
+
+    void Update()
+    {
+        float dist = Vector3.Distance(PinMarker.position, BallMarker.position);
+        distance.text = dist + "";
+        if(ballMarkerScript.Visible==true)
+        {
+            twoMarkers.text = "Hiro";
+        }
+        else
+        {
+            twoMarkers.text = "";
+        }
+    }
+
+    private void GetMarkers()
+    {
+        for(int i =0; i<armarker.Length;i++)
+        {
+            if(armarker[i].Tag == "hiro")
+            {
+                ballMarkerScript = armarker[i];
+            }
+
+            if(armarker[i].Tag == "kanji")
+            {
+                pinMarkerScript = armarker[i];
+            }
+        }
+    }
+}
