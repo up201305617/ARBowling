4da97ba866d876fc019aff0187de9c05857af973
Jorge <up201305617@fe.up.pt>
2017-10-13 11:50:04 +0100

    throw ball. second mark working

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-	void Start ()
+    private Rigidbody rb;
+    private Vector3 velocity;
+
+    void Start ()
     {
-		
+        rb = GetComponent<Rigidbody>();
+        velocity = new Vector3(0, 0, 0.5f);
 	}
 	
 	void Update ()
     {
 		
 	}
+
+    void FixedUpdate()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            rb.velocity = velocity;
+    }
 
     public void RightArrow()
     {
