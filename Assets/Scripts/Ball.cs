d55809d61837d335ea8d42f600b29d4ed222254b
Jorge <up201305617@fe.up.pt>
2017-10-13 09:28:44 +0100

    Moving ball left and right

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void RightArrow()
+    {
+        this.transform.Translate(new Vector3(+0.01f, 0, 0));
+    }
+
+    public void LeftArrow()
+    {
+        this.transform.Translate(new Vector3(-0.01f, 0, 0));
+    }
+}
