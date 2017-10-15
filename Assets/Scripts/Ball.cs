0b2db92be2e8a6aadf1acf60c12d58744720c394
Jorge <up201305617@fe.up.pt>
2017-10-15 19:48:29 +0100

    adding power to ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,12 +7,14 @@ public class Ball : MonoBehaviour
     private Rigidbody rb;
     private Vector3 velocity;
     private Vector3 initialPosition;
+    private Check check;
 
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
         initialPosition = transform.position;
         velocity = new Vector3(0, 0, 1f);
+        check = FindObjectOfType<Check>();
 	}
 	
 	void Update ()
@@ -38,12 +40,15 @@ public class Ball : MonoBehaviour
 
     public void LaunchBall()
     {
+        velocity = velocity * check.power.value;
         rb.velocity = velocity;
     }
 
     public void Reset()
     {
         rb.velocity = Vector3.zero;
+        velocity = new Vector3(0, 0, 1);
         transform.position = initialPosition;
+        check.power.value = 0;
     }
 }
