690b13afa93bc816764814799f559fb443c4107f
Jorge <up201305617@fe.up.pt>
2017-11-07 17:05:28 +0000

    nivel dificuldade

---
+++ Assets/Scripts/MovingBlock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBlock : MonoBehaviour
+{
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        transform.position = new Vector3(Mathf.PingPong(Time.time/2, 0.4f)-0.1f, transform.position.y, transform.position.z);
+    }
+}
