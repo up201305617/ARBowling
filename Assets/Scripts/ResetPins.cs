67935e2d3e21a7bf9b655acab330dc25d250d7cf
Jorge <up201305617@fe.up.pt>
2017-11-07 16:15:57 +0000

    undo - donw

--- Assets/Scripts/ResetPins.cs
+++ Assets/Scripts/ResetPins.cs
@@ -4,51 +4,19 @@ using UnityEngine;
 
 public class ResetPins : MonoBehaviour
 {
-    public GameObject pin_1;
-    public GameObject pin_2;
-    public GameObject pin_3;
-    public GameObject pin_4;
-    public GameObject pin_5;
-    public GameObject pin_6;
-    public GameObject pin_7;
-    public GameObject pin_8;
-    public GameObject pin_9;
-    public GameObject pin_10;
-
-    private Transform original1;
-    private Transform original2;
-    private Transform original3;
-    private Transform original4;
-    private Transform original5;
-    private Transform original6;
-    private Transform original7;
-    private Transform original8;
-    private Transform original9;
-    private Transform original10;
     private Transform temp;
     private bool firstTime;
-    public GameObject pinPrefab;
-    public GameObject prefab;
-    public GameObject old;
-    public Transform marker;
+    public GameObject prefabPinSet;
+    public GameObject oldPinSet;
+    public Transform markerPosition;
 
     void Start()
     {
         firstTime = true;
-        original1 = pin_1.transform;
-        original2 = pin_2.transform;
-        original3 = pin_3.transform;
-        original4 = pin_4.transform;
-        original5 = pin_5.transform;
-        original6 = pin_6.transform;
-        original7 = pin_7.transform;
-        original8 = pin_8.transform;
-        original9 = pin_9.transform;
-        original10 = pin_10.transform;
 
         if (firstTime)
         {
-            temp = old.transform;
+            temp = oldPinSet.transform;
             firstTime = false;
         }
     }
@@ -60,8 +28,8 @@ public class ResetPins : MonoBehaviour
 
     public void Restart()
     {
-        Destroy(old);
-        old = (GameObject)Instantiate(prefab, marker.position, Quaternion.Euler(90, 0, 0));
+        Destroy(oldPinSet);
+        oldPinSet = (GameObject)Instantiate(prefabPinSet, markerPosition.position, Quaternion.Euler(90, 0, 0));
         Check.pinsStanding = 0;
     }
 }
